4b85a0821d5e71fb5b384539f61aac9f71a14b5e
Haining Pan <jack_pan1994@126.com>
2022-03-22 21:53:59 -0400

    Add CloudConvert API

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Project: eProof helper
-This repository is meant to help eProof easier when the manuscript is under the production process. The working scenario would be when the manuscript is returned after modified by the copyeditor with redlines indicating the changes which are usually not very perceivable. (See Before below)
+This repository is meant to help eProof easier when the manuscript is under the production process. The working scenario would be when the manuscript is returned after being modified by the copyeditor with redlines indicating the changes which are usually not very perceivable. (See Before below)
 
 This snippet can help eProof a bit easier by (1) converting the thin strikethroughs into background colors; (2) adding a colored block before each line whenever there is a change in that line (red indicates deletions, blue indicates addition). (See After below) 
 
@@ -10,14 +10,22 @@ After that, the authors should catch the changes more easily, which may be furth
 | ![original](original.png)| ![converted](converted.png)|
 
 
-# Methods
-1. Convert the redline PDF file to html. [https://cloudconvert.com/pdf-converter]
-2. `python eproof.py`, then enter the path of the HTML file. The filename of the converted HTML will be appended `_mod`.
+# Usage
+`python eproof.py -fn pathtofile.pdf` to convert `pathtofile.pdf` into `pathtofile.html`.
 
+
+# Methods
+1. Convert the redline PDF file to HTML by calling the [API of CloudConvert](https://cloudconvert.com/pdf-converter).
+2. Increase the visibility on the HTML file.
+
+# Help
+`python eproof.py -h`
 
 # To Do
-1. Remove the mysterious blue block at the end of each page.
-2. When the equations are rendered in red, the fraction lines and square root signs may be misidentified as strikethroughs. 
-
+- [ ] Remove the mysterious blue block at the end of each page.
+- [ ] When the equations are rendered in red, the fraction lines and square root signs may be misidentified as strikethroughs. 
+- [ ] Add docstrings
+ 
 # Note
-The template is the APS journals such as [Physical Reviews Letters](https://journals.aps.org/elecproofs.html).  
+* The template is the APS journals such as [Physical Reviews Letters](https://journals.aps.org/elecproofs.html).
+* Token (`self.api_key`) can be modified at [https://cloudconvert.com/dashboard/api/v2/keys](https://cloudconvert.com/dashboard/api/v2/keys)
--- eproof.py
+++ eproof.py
@@ -3,6 +3,9 @@ from PIL import Image
 from io import BytesIO
 import base64
 import numpy as np
+import os
+import argparse
+import requests
 
 def shift_image(src_image,dst_image,direction,color,transparency=100):
     '''
@@ -47,21 +50,94 @@ def image_proc(original_img_data):
     new_img_url = 'data:image/jpg;base64,' + new_img_encode
     return new_img_url
 
-def read_html():
-    print('type HTML filename:')
-    filename=input()
-    with open(filename, 'r') as f:
-        html_doc = f.read()
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    imgtext_list=soup.find_all('img')[:-1]
-    for index,imgtext in enumerate(imgtext_list):
-        print('Processing Page {:d}/{:d}...'.format(index+1,len(imgtext_list)))
-        original_img_data=imgtext['src']
-        original_img_data=original_img_data.replace('data:image/png;base64,','')
-        new_img_data=image_proc(original_img_data)
-        imgtext['src']=new_img_data
-    with open(filename.replace('.html','_mod.html'),'w',encoding='utf-8') as f:
-        f.write(str(soup))
+class Convert:
+    def __init__(self,filename):
+        self.filename=filename
+        filename_list=filename.split('.')
+        assert len(filename_list)>1, 'The file ({}) does not have an extension.'.format(filename)
+        self.file='.'.join(filename_list[:-1])
+        self.ext=filename_list[-1]
+        assert self.ext.lower()=='pdf', 'The file should be PDF.'
+        self.api_key='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMmY1NjFjZThjYzgxZWMxMmYxYTJhZGUyMjEyZGI5ZTJkYWViNzViZGM1ZjNkNmVmYWMxZjUxYTAwNjY4MjkxZjQ5MmQ4ZmMwOGQ1Yzg3MjEiLCJpYXQiOjE2NDgwMDAwMDQuMDEzMjQ4LCJuYmYiOjE2NDgwMDAwMDQuMDEzMjUsImV4cCI6NDgwMzY3MzYwNC4wMDk1Niwic3ViIjoiNTY5MTU4OTQiLCJzY29wZXMiOlsidXNlci5yZWFkIiwidXNlci53cml0ZSIsInRhc2sucmVhZCIsInRhc2sud3JpdGUiLCJ3ZWJob29rLnJlYWQiLCJ3ZWJob29rLndyaXRlIiwicHJlc2V0LnJlYWQiLCJwcmVzZXQud3JpdGUiXX0.W4N0BBqxoQ6ex3TABRgs_DPcPHHxmtDk7SNGdfNzMPbajPL-HbPBrVailGajSLmpGiQO14TcQdNZ12id3fVet5CLrB4ncXwKpRgdU6GnFMNCgEgevGJpW3Vsjosnvau75reQbNgCxZU4FkePy4dg6UbHCDDNAhnRq-NtwC5tUnI4hQzMAlLoVeke3A_ahDEmrea1d0Xgzb_nVBi0eCFnEH61ggtiTkz2eGFNdifVnUCajfGpYZFc1rpC_qvwdKx5WPZmTh2Q9naB-xWn3JPJEKJyIN0yPSTmHjoJbwtlTkI_ZIsd_iWB-NXpCxomXsR_t7BpvfaW9ShTPYa0xV35W26Lr9YAUWXP5wnGTvyg4BPRT89C87xdRjXKmbKkuicZWA4GgZ2o9BLCWLM6IO3aRETUFobk0rhf2iu0HZRwQz3hHgMTNz2wMsP3HqTSEwg75Q4qv_r_qjoj4SmbQJ4UIT_7uZOFJaz4OnuOxmgAyGJ40Q4ZwyeEgs24aZiY8e0B42IRVTFofNzarZV0krHJ0xSa2xxK9_i60xdnnvH8VcGlCGvucBkbq8xUbZq7b5p3YZt0ELrbeKkHo8k_j3VAsMDqFzc0j92Hfr_KyLS4VeR5l_b5Oh7dx4FaN748fCtw1sKakjYOmSsXtByuHv07hZycdu_1Nv5hjLU6oym6o08'
+        self.headers={'Authorization':'Bearer {}'.format(self.api_key),'Content-type': 'application/json'}
+
+    def submit_job(self):
+        body={
+            "tasks": {
+                "import-1": {
+                    "operation": "import/upload",
+                },
+                "task-1": {
+                    "operation": "convert",
+                    "input_format": "pdf",
+                    "output_format": "html",
+                    "engine": "pdf2htmlex",
+                    "input": [
+                        "import-1"
+                    ],
+                    "outline": False,
+                    "zoom": 1.5,
+                    "embed_css": True,
+                    "embed_javascript": True,
+                    "embed_images": True,
+                    "embed_fonts": True,
+                    "split_pages": False,
+                    "bg_format": "png"
+                },
+                "export-1": {
+                    "operation": "export/url",
+                    "input": [
+                        "task-1"
+                    ],
+                    "inline": False,
+                    "archive_multiple_files": False
+                }
+            },
+            "tag": "jobbuilder"
+        }        
+        self.response_submit = requests.post('https://api.cloudconvert.com/v2/jobs',json=body,headers={'Authorization':'Bearer {}'.format(self.api_key)})
+        
+
+    def upload(self):
+        response_submit_dict=self.response_submit.json()
+        try:
+            self.job_id=response_submit_dict['data']['id']
+            form=response_submit_dict['data']['tasks'][0]['result']['form']
+        except:
+            raise ValueError(response_submit_dict['message'])
+        port_url=form['url']
+        params=form['parameters']
+        file=open(self.filename,'rb')
+        files={'file':file}
+        requests.post(port_url,files=files,data=params)
+        file.close()
+        print('Converting to html...')
+
+    def export(self):
+        finished=requests.get('https://api.cloudconvert.com/v2/jobs/{}/wait'.format(self.job_id),headers=self.headers)
+        download_url=finished.json()['data']['tasks'][0]['result']['files'][0]['url']
+        download_filename=finished.json()['data']['tasks'][0]['result']['files'][0]['filename']
+        self.downloads=requests.get(download_url)
+
+    def read_html(self):
+        html_doc=self.downloads.content.decode('utf-8')
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        imgtext_list=soup.find_all('img')[:-1]
+        for index,imgtext in enumerate(imgtext_list):
+            print('Processing Page {:d}/{:d}...'.format(index+1,len(imgtext_list)))
+            original_img_data=imgtext['src']
+            original_img_data=original_img_data.replace('data:image/png;base64,','')
+            new_img_data=image_proc(original_img_data)
+            imgtext['src']=new_img_data
+        with open('{}.html'.format(self.file),'w',encoding='utf-8') as f:
+            f.write(str(soup))
 
 if __name__=="__main__":
-    read_html()
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-fn',type=str,help='File name')
+    args=parser.parse_args()
+    convert=Convert(args.fn)
+    convert.submit_job()
+    convert.upload()
+    convert.export()
+    convert.read_html()
